8012c936abf1bfe20f591eaf81dfc30873184eaf
Nurtas <nurtaskalmakhan>
2022-10-16 00:37:15 +0200

    hhhl

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!jjjjjj");
+        System.out.println("Hello world!ljjjjjj");
         System.out.println("Hello world!jjjjljj");
     }
 }
